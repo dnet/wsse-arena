d95796268a3aa79c40fcd84151ff039e3b928333
András Veres-Szentkirályi <vsza@vsza.hu>
2011-11-11 13:24:47 +0100

    suds: added UsernameToken handling

--- suds/client.py
+++ suds/client.py
@@ -4,4 +4,12 @@ from suds import client
 from os import environ
 
 c = client.Client(environ['WSDL_URL'])
+
+if 'SECURE' in environ:
+    from suds.wsse import Security, UsernameToken
+    security = Security()
+    token = UsernameToken('admin', 'nimda', 'PLAIN' not in environ)
+    security.tokens.append(token)
+    c.set_options(wsse = security)
+
 print c.service.say_hello('World', 3)
