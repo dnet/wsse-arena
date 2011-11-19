2c58980f2cd466773f11f003fdf0c5fa4dcdff1e
András Veres-Szentkirályi <vsza@vsza.hu>
2011-11-19 21:25:55 +0100

    suds: added signature creation

--- suds/client.py
+++ suds/client.py
@@ -3,9 +3,14 @@
 from suds import client
 from os import environ
 
-c = client.Client(environ['WSDL_URL'])
+plugins = []
+if 'SIGN' in environ:
+	from SudsSigner.plugin import SignerPlugin
+	plugins.append(SignerPlugin('../keys/privkey.pem'))
 
-if 'SECURE' in environ:
+c = client.Client(environ['WSDL_URL'], plugins=plugins)
+
+if 'SECURE' in environ and 'SIGN' not in environ:
     from suds.wsse import Security, UsernameToken
     security = Security()
     token = UsernameToken('admin', 'nimda', 'PLAIN' not in environ)
